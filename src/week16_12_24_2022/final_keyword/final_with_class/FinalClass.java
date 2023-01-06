d660c79613887618047f6c07007c2008e6b33247
Mustafa <mustafamuezzinoglu@gmail.com>
2023-01-06 18:21:08 +0300

    week 16 Final Keyword done

---
+++ src/week16_12_24_2022/final_keyword/final_with_class/FinalClass.java
@@ -0,0 +1,13 @@
+package week16_12_24_2022.final_keyword.final_with_class;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class FinalClass {
+    public static void main(String[] args) {
+        
+//    String Class // is Final
+//    Arrays  // is not Final
+//     Array // is Final
+    }
+}
---
+++ src/week16_12_24_2022/final_keyword/final_with_class/Phone.java
@@ -0,0 +1,6 @@
+package week16_12_24_2022.final_keyword.final_with_class;
+
+public class Phone {
+    String brand;
+    String model;
+}
